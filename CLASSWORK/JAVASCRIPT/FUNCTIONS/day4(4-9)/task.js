a3b82444d0b7037ce9f76567c0f3ebca4d280d24
an9644 <5unni4plr@gmail.com>
2024-10-07 21:11:35 +0530

    commit

--- CLASSWORK/JAVASCRIPT/FUNCTIONS/day4(4-9)/task.js
+++ CLASSWORK/JAVASCRIPT/FUNCTIONS/day4(4-9)/task.js
@@ -1,4 +1,4 @@
-let task =[];
+let tasks =[];
 let priorities=[];
 
 function addtask(){
@@ -12,7 +12,7 @@ function addtask(){
 
     if(task!=='' && !isNaN(priority) && priority>=1 && priority<=3){
         tasks.push(task);
-        priorities =push(priority);
+        priorities.push(priority);
 
         const li= document.createElement('li');
         li.textContent=task;
@@ -24,24 +24,24 @@ function addtask(){
             case 2:
             li.classList.add('priority.medium')
             break;
-            case 3:
+            case 3: 
             li.classList.add('priority.low')
             break;
         }
         const completebutton= document.createElement('button')
         completebutton.textContent='complete'
         completebutton.onclick=function(){
-            li.classList.toggle=['completed']
+            li.classList.toggle('completed')
         };
         li.appendChild(completebutton);
-
+                                                                     
         const editbutton= document.createElement('button')
         editbutton.textContent='Edit'
         editbutton.onclick= function(){
             const newtask=prompt('Edit task: ',task)
             if(newtask!==null && newtask.trim()!==''){
-                const taskindex=tasklist.indexof(task)
-                tasklist[taskindex]=newtask;
+                const taskindex=tasks.indexof(task)
+                tasks[taskindex]=newtask;
                 li.firstChild.textContent=newtask
                 task=newtask
         }
@@ -49,14 +49,14 @@ function addtask(){
     li.appendChild(editbutton);
         const removebutton = document.createElement('button')
         removebutton.textContent='Remove'
-
         removebutton.onclick= function(){
             tasklist.removeChild(li)
-            const taskindex= task.indexof(task)
-            tasklist.splice(taskindex,1)
-            priority.splice(taskindex,1)
+            const taskindex= tasks.indexof(task)
+            tasks.splice(taskindex,1)
+            priorities.splice(taskindex,1)
         };
         li.appendChild(removebutton)
+
         tasklist.appendChild(li)
         taskinput.value=''
         priorityinput.value=' '
